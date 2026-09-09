e679efd17cb36ff8747ab684b4bd17055171517f
user2015 <slbproductions3@gmail.com>
2026-09-09 11:08:24 -0600

    Another change: "Hello World!".

--- dmit2015-javase-demo/src/main/java/dmit2015/Main.java
+++ dmit2015-javase-demo/src/main/java/dmit2015/Main.java
@@ -1,12 +1,12 @@
 package dmit2015;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
+    
+
+
+
 }
