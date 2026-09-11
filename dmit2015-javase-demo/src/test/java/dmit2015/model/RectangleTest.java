6678cc9ffbe2959ac2ac206d233e21c75c682fb7
user2015 <slbproductions3@gmail.com>
2026-09-10 19:21:58 -0600

    Put code for a practice test I'll try tomorrow.

---
+++ dmit2015-javase-demo/src/test/java/dmit2015/model/RectangleTest.java
@@ -0,0 +1,10 @@
+package dmit2015.model;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+public class RectangleTest {
+    @Test
+    public void testArea() {}
+}
+//Umm...
