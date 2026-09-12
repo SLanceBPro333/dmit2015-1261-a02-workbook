2c727b7d0e8df154d93b1252a9333f1891565482
user2015 <slbproductions3@gmail.com>
2026-09-12 15:31:11 -0600

    Put some code in the RectangleTest.java.

--- dmit2015-javase-demo/src/test/java/dmit2015/model/RectangleTest.java
+++ dmit2015-javase-demo/src/test/java/dmit2015/model/RectangleTest.java
@@ -1,17 +1,20 @@
 package dmit2015.model;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.data.Offset.offset;
 
 public class RectangleTest {
+
     @Test
     void testRectangle() {
-
         Rectangle rectangle = new Rectangle();
         rectangle.setLength(5);
         rectangle.setWidth(5);
 
-        assertThat(rectangle.area()).isCloseTo(78.54, within(0.01));
-        assertThat(rectangle.perimeter()).isCloseTo(78.54, within(0.01));
+        assertThat(rectangle.area()).isCloseTo(25.0, offset(0.01));
+        assertThat(rectangle.perimeter()).isCloseTo(20.0, offset(0.01));
     }
 }
+
