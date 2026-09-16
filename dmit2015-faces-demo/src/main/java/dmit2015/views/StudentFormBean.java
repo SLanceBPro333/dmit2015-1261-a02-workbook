237b6e8be7c39dec55d05f2da6284b26d542526a
user2015 <slbproductions3@gmail.com>
2026-09-16 11:48:09 -0600

    James...

---
+++ dmit2015-faces-demo/src/main/java/dmit2015/views/StudentFormBean.java
@@ -0,0 +1,60 @@
+package dmit2015.views;
+
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import org.omnifaces.cdi.ViewScoped;
+
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class StudentFormBean implements Serializable {
+
+    private int submissionCount;
+    private String fullName;
+    private String program;
+    private boolean fullTime;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public boolean isFullTime() {
+        return fullTime;
+    }
+
+    public void setFullTime(boolean fullTime) {
+        this.fullTime = fullTime;
+    }
+
+    public int getSubmissionCount() {
+        return submissionCount;
+    }
+
+    public void submit() {
+        submissionCount++;
+
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Form Submitted: ",
+                String.format("Welcome %s to %s program (%s)", fullName, program, fullTime ? "Full Time" : "Part Time"));
+
+        FacesContext.getCurrentInstance().addMessage(null, message);
+
+        fullName = null;
+        program = null;
+        fullTime = true;
+
+    }
+}
