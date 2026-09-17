e69935988faff364a5768fefc16bfec218ca33da
user2015 <slbproductions3@gmail.com>
2026-09-17 11:52:27 -0600

    More...

--- dmit2015-faces-demo/src/main/java/dmit2015/views/StudentFormBean.java
+++ dmit2015-faces-demo/src/main/java/dmit2015/views/StudentFormBean.java
@@ -1,5 +1,6 @@
 package dmit2015.views;
 
+import dmit2015.model.StudentInfo;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
@@ -12,49 +13,30 @@ import java.io.Serializable;
 public class StudentFormBean implements Serializable {
 
     private int submissionCount;
-    private String fullName;
-    private String program;
-    private boolean fullTime;
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getProgram() {
-        return program;
-    }
-
-    public void setProgram(String program) {
-        this.program = program;
-    }
-
-    public boolean isFullTime() {
-        return fullTime;
-    }
-
-    public void setFullTime(boolean fullTime) {
-        this.fullTime = fullTime;
-    }
 
     public int getSubmissionCount() {
         return submissionCount;
+    }
+
+    private StudentInfo studentInfo= new StudentInfo();
+
+    public StudentInfo getStudentInfo() {
+        return studentInfo;
+    }
+
+    public void setStudentInfo(StudentInfo studentInfo) {
+        this.studentInfo = studentInfo;
     }
 
     public void submit() {
         submissionCount++;
 
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Form Submitted: ",
-                String.format("Welcome %s to %s program (%s)", fullName, program, fullTime ? "Full Time" : "Part Time"));
+                String.format("Welcome %s to %s program (%s)", studentInfo.getFullName(), studentInfo.getProgram(), studentInfo.isFullTime() ? "Full Time" : "Part Time"));
 
         FacesContext.getCurrentInstance().addMessage(null, message);
 
-//        fullName = null;
-//        program = null;
-//        fullTime = true;
+        studentInfo = new StudentInfo();
 
     }
 }
